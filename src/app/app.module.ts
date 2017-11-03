864d3b35a550dde9ade62f2d78a48b5362e2dddb
André de Sousa Costa filho <dedaoskater@hotmail.com>
2017-11-03 01:36:19 -0200

    aula 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
+  // aqui ficam as declarações dos componentes que serão utilizados
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuSegundoComponent
+    MeuSegundoComponent,
   ],
+  // aqui se colocam os modules que serão importados para o uso
+  // os modules daqui tem tudo visibilidade máxima
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
+  // seção para linkar com os services
   providers: [],
+  // so tem na AppComponent, define a base da aplicação
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  // declarando os modules que serão usados aqui na module,
+  // se declarados so aqui ficam kinda 'private'
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent
+  ],
+  // expondo a classe dos componentes para outros modules
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
